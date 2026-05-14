f5a0967004bf2b83ca4f0c1b9e11f37c39c78407
NotBrian122 <s00254527@atu.ie>
2026-05-14 09:37:56 +0100

    first commit

---
+++ src/app/components/list-countries/list-countries.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-countries',
+  imports: [],
+  templateUrl: './list-countries.html',
+  styleUrl: './list-countries.css',
+})
+export class ListCountries {}
