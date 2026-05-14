138c3dfa425b901669afbeec54ecf801c5975e9d
NotBrian122 <s00254527@atu.ie>
2026-05-14 09:51:15 +0100

    Bootstrap and routes now wokring

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListCountries } from './components/list-countries/list-countries';
+import { About } from './components/about/about';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: 'list-countries',component:ListCountries},
+    {path: 'about', component:About}
+];
