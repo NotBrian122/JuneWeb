c5c7c7bdefcd820cd82a4f2d5e14ccc45793ef4e
NotBrian122 <s00254527@atu.ie>
2026-05-14 10:56:40 +0100

    trying to get the country detials in said list compoent

--- src/app/components/list-countries/list-countries.ts
+++ src/app/components/list-countries/list-countries.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { ExamApi } from '../../services/exam-api';
+import { OnInit } from '@angular/core';
+import { Countries } from '../../CountriesDetails.interface';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-list-countries',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './list-countries.html',
   styleUrl: './list-countries.css',
 })
-export class ListCountries {}
+export class ListCountries {
+countryDetials: any;
+
+constructor(public countryAPIService: ExamApi){
+this.countryDetials = this.countryAPIService.getCountries();
+console.log('you telling me this does pass',this.countryDetials)
+}
+}
--- src/app/services/exam-api.ts
+++ src/app/services/exam-api.ts
@@ -1,6 +1,29 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { Countries } from '../CountriesDetails.interface';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { take } from 'rxjs';
+import { Inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ExamApi {}
+export class ExamApi {
+  //this is the public property of signal thats a list of countries
+  public countries = signal<Countries[]>([]);
+  private http = inject(HttpClient);
+  //private apiUrl
+  private apiUrl = 'https://restcountries.com/v3.1/all?fields=name,capital,flags,independent';
+
+
+  getCountries(){
+   this.http.get<any>(this.apiUrl)
+  .pipe(take(1))
+    .subscribe(
+      (countryData) => {
+      this.countries.set(countryData);
+      console.log('country:', countryData);
+      }
+    );
+  }
+}
